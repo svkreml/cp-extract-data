44eac6703ca3833b9417af06128684e7a32b938a
svkreml <svkreml@gmail.com>
2020-08-02 23:42:59 +0300

    форматирование

--- ExtractPkey2/Program.cs
+++ ExtractPkey2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Mono.Options;
 
 namespace ExtractPkey
@@ -65,13 +66,21 @@ namespace ExtractPkey
             else
                 export = new PrivateKeyExport();
 
-            try
-            {
-                export.Export(container, Console.OpenStandardOutput());
-            } catch (Exception e)
-            {
-                Console.Error.WriteLine(e.Message);
-            }
+        
+                //  Stream openStandardOutput = Console.OpenStandardOutput();
+                MemoryStreamA mem = new MemoryStreamA();
+                
+                mem.Close();
+                    export.Export(container, mem);
+                    
+                    using (StreamReader streamReader = new StreamReader(mem))
+                    {
+                        mem.Position = 0;
+                        string result = streamReader.ReadToEnd();
+                        Console.WriteLine(result);
+                        mem.CloseReal();
+                    } 
+            
         }
 
         private static void PrintHelp()
@@ -88,5 +97,20 @@ namespace ExtractPkey
             Private,
             Certificate
         }
+    }
+    
+    class MemoryStreamA : MemoryStream
+    {
+
+        public override void Close()
+        {
+           // base.Close();
+        }
+
+        public void CloseReal()
+        {
+             base.Close();
+        }
+
     }
 }
